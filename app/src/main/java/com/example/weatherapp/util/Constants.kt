1c7fc00e4ba289f50df7cb7bda04b9215986c057
ishbo <adeswal09@gmail.com>
2025-09-08 18:49:58 +0530

    Removing the API key

--- app/src/main/java/com/example/weatherapp/util/Constants.kt
+++ app/src/main/java/com/example/weatherapp/util/Constants.kt
@@ -1,6 +1,6 @@
 package com.example.weatherapp.util
 
 object Constants {
-    const val OPENWEATHER_API_KEY = "5e47495ce3cc4e89be32389a39008aaf" // 🔑 your key here
+    const val OPENWEATHER_API_KEY = "Enter your key" 
     const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
 }
